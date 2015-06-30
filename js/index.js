deef979ea007aa52c34ce6daf611f50fc35fcbc6
amyleewalton <amylee@mapbox.com>
2015-06-29 20:07:47 -0400

    re-starting point

--- js/index.js
+++ js/index.js
@@ -1,7 +1,26 @@
 var map = L.map('map', { scrollWheelZoom: false });
 
+/*
 
-L.tileLayer('https://a.tiles.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=' + LRM.apiToken, {
+var defaultView = {
+  center: L.latLng(38.8995,-77.0269),
+  zoom: 13,
+  waypoints: [],
+  language: 'en',
+  service: 'Car (fastest)',
+  layer: 'Mapbox Emerald',
+};
+
+var defaultControl = {
+  geocoder: L.Control.Geocoder.nominatim(),
+  routeWhileDragging: true,
+  routeDragInterval: 2,
+  addWaypoints: false,
+  waypointMode: 'snap',
+};
+
+*/
+L.tileLayer('https://a.tiles.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}@2x.png?access_token=' + LRM.apiToken, {
 	attribution: 'Maps by <a href="https://www.mapbox.com/about/maps/">MapBox</a>. ' +
 		'Routes from <a href="http://project-osrm.org/">OSRM</a>, ' +
 		'data uses <a href="http://opendatacommons.org/licenses/odbl/">ODbL</a> license'
